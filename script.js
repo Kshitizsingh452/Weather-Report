b1c13f112a142f688c9eb788044a5cac68d4c02d
iamkshitizkumarsingh <kshitizsingh244@gmail.com>
2021-12-02 14:06:19 +0530

    started js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-7266d3292fe8c24e52d6ef8ea382a4e1
 
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const weatherAPI={
+key:"7266d3292fe8c24e52d6ef8ea382a4e1",
+baseUrl:"https://api.openweathermap.org/data/2.5/weather?"
+}
+
+const searchInputBox=document.getElementById('input-box');
+searchInputBox.addEventListener('keypress',(event) =>{
+    if(event.keyCode==13){
+    console.log(searchInputBox.value);
+       
+    }
+});
+
+
+
+
+
