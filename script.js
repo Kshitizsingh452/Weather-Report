565a6b2459e047493361a922e16c4c6cce6afa2f
iamkshitizkumarsingh <kshitizsingh244@gmail.com>
2021-12-02 19:09:54 +0530

    js completed

--- script.js
+++ script.js
@@ -38,52 +38,65 @@ function weather_report(city) {
     .then((json) => show(json));
 }
 
-function show(json) {
-  console.log(json);
-  let city=document.getElementById('city');
-  city.innerText=`${weather.name},${weather.sys.country}`;
+function show(weather) {
+  console.log(weather);
+  let city = document.getElementById("city");
+  city.innerText = `${weather.name},${weather.sys.country}`;
 
-  let temp=document.getElementById('temp');
-  temp.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
+  let temp = document.getElementById("temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
 
-  let min_max=document.getElementById('min-max');
-  min_max.innerHTML=`${Math.floor(weather.main.temp_min)}&deg;C (min)/  ${Math.ceil(weather.main.temp_max)}&deg;C(max) `;
-  let weatherType=document.getElementById('weather');
-  weatherType.innerText=`${weather.weather[0].main}`;
-  let date=document.getElementById('date');
-  let todayDate=new Date();
-  date.innerText=dateManage(todayDate);
+  let min_max = document.getElementById("min-max");
+  min_max.innerHTML = `${Math.floor(
+    weather.main.temp_min
+  )}&deg;C (min)/  ${Math.ceil(weather.main.temp_max)}&deg;C(max) `;
+  let weatherType = document.getElementById("weather");
+  weatherType.innerText = `${weather.weather[0].main}`;
+  let date = document.getElementById("date");
+  let todayDate = new Date();
+  date.innerText = dateManage(todayDate);
 
-  if(weatherType.textContent=='Clear')
-  {
-      document.body.style.backgroundImage="url(./images/clear.webp)";
-  }else if(weatherType.textContent=='Clouds')
-  {
-      document.body.style.backgroundImage="url(./images/clody.webp)";
-  }else if(weatherType.textContent=='Rain')
-  {
-      document.body.style.backgroundImage="url(./images/rain.webp)";
-  }else if(weatherType.textContent=='Snow')
-  {
-      document.body.style.backgroundImage="url(./images/snow.webp)";
-  }else if(weatherType.textContent=='Thunderstrom')
-  {
-      document.body.style.backgroundImage="url(./images/thunder.webp)";
+  if (weatherType.textContent == "Clear") {
+    document.body.style.backgroundImage = "url(./images/clear.webp)";
+  } else if (weatherType.innerText == "Clouds") {
+    document.body.style.backgroundImage = "url(./images/clody.webp)";
+  } else if (weatherType.textContent == "Rain") {
+    document.body.style.backgroundImage = "url(./images/rain.webp)";
+  } else if (weatherType.textContent == "Snow") {
+    document.body.style.backgroundImage = "url(./images/snow.webp)";
+  } else if (weatherType.textContent == "Thunderstrom") {
+    document.body.style.backgroundImage = "url(./images/thunder.webp)";
   }
-
 }
 
+function dateManage(dateArg) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedenesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let year = dateArg.getFullear();
+  let month = months[dateArg.getMonth()];
+  let date = dateArg.getDate();
+  let day = days[dateArg.getDay()];
 
-function dateManage(dateArg){
-    let days=["Sunday","Monday","Tuesday","Wedenesday","Thursday","Friday","Saturday"];
-    let months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-    let year=dateArg.getFullear();
-    let month=months[dateArg.getMonth()];
-    let date=dateArg.getDate();
-    let day=days[dateArg.getDay()];
-
-    return `${date}, ${month},${day},${year}`;
-
-
+  return `${date}, ${month},${day},${year}`;
 }
-
